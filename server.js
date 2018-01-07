2748e6eccea4e0c3244119ffd5cada7583979641
lesrel <sebastien.lesrel@gmail.com>
2018-01-07 17:49:52 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+app.use((req,res,next) => {
+  res.render('Maintenance.hbs', {
+    head: 'Maintenance Page',
+    pageTitle: 'Maintenance Page'
+  });
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+});
+
+hbs.registerHelper('jsonP', (objet) => {
+  return JSON.stringify(objet, undefined, 2);
+});
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res) => {
+  res.render('Hello.hbs', {
+    head: 'Hello Page 123',
+    pageTitle: 'Hello Page',
+    name: 'Seb',
+    likes: {likes:['Computer science','Cinema','Restaurant','Science Fiction']}
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
